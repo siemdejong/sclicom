ae0c56774ee72f7960c60bc31266396553f2da32
semantic-release <semantic-release>
2023-02-13 15:39:04 +0100

    0.1.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup  # type: ignore
+
+__version__ = "0.1.0"
+
+# Get the long description from the README file
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     author="Siem de Jong",
-    name="dpat",
-    packages=find_packages(include=["dpat", "dpat.*"]),
+    author_email="siem.dejong@hotmail.nl",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
     entry_points={
         "console_scripts": [
             "dpat=dpat.cli:main",
         ],
     },
+    install_requires=[
+        "dlup>=0.3",
+    ],
+    license="GNU General Public License v3",
+    keywords="dpat",
+    name="dpat",
+    packages=find_packages(include=["dpat", "dpat.*"]),
+    url="https://github.com/siemdejong/hhg-dpat",
+    version=__version__,
 )
