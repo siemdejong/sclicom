37e0c52dc200bd72093c89aabc6016ec44cf7d60
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2023-03-16 12:20:39 +0100

    fix: rename *_epoch_end to on_*_epoch_end
    *_epoch_end is deprecated in lightning 2

--- dpat/mil/models/varmil.py
+++ dpat/mil/models/varmil.py
@@ -223,12 +223,12 @@ class Attention(pl.LightningModule):
             ) as f:
                 f.write(json.dumps(metrics_to_save))
 
-    def validation_epoch_end(self, validation_step_outputs) -> None:
+    def on_validation_epoch_end(self) -> None:
         """Procedure to run at the end of a validation epoch."""
         self.log_metrics(prefix="val", output=self.validation_output)
         self.validation_output = self._reset_output()
 
-    def test_epoch_end(self, test_step_outputs) -> None:
+    def on_test_epoch_end(self) -> None:
         """Procedure to run at the end of a test epoch."""
         self.log_metrics(prefix="test", output=self.test_output)
         self.test_output = self._reset_output()
