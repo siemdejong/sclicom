d0d28e6a3a04d9f76d155a226c6b1ff91d8380e1
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2023-04-06 13:58:16 +0200

    fix: remove argument linking

--- dpat/mil/trainer/mil_trainer.py
+++ dpat/mil/trainer/mil_trainer.py
@@ -8,9 +8,3 @@ class MILTrainer(pl.Trainer):
     def __init__(self, *args, **kwargs):
         """Initialize Pytorch Lightning Trainer."""
         super().__init__(*args, **kwargs)
-
-    def add_arguments_to_parser(self, parser):
-        """Add arguments to parser and link configurations."""
-        parser.link_arguments(
-            "trainer.max_epochs", "model.init_args.scheduler.init_args.T_max"
-        )
