d1a1da8fe8bdf79e0fe48e6426e004b45325f80c
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2023-04-12 15:02:58 +0200

    docs: remove _static rom html_static_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,4 +20,4 @@ templates_path = ["_templates"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []
