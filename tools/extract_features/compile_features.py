fix: use normalization based on masked tiles

tools/extract_features/compile_features.py
@@ -3,21 +3,24 @@ import logging
 import pathlib
 import tempfile
 
+import torch
+from torchvision.transforms import Compose, Normalize, ToTensor  # isort: ignore
+
 # The pyvips import from dlup (dpat dependency) conflicts with torchvision.
 # Import dpat first.
-from dpat.data import PMCHHGH5Dataset, PMCHHGImageDataset  # isort: skip
-from dpat.extract_features.models import SwAV, SimCLR  # isort: skip # noqa: F401
-
-from torchvision.transforms import Compose, ToTensor  # isort: skip
+from dpat.data import PMCHHGH5Dataset, PMCHHGImageDataset
+from dpat.extract_features.models import SimCLR
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     """Compile features using a trained model and a datamodule."""
-    model = SimCLR.load_from_checkpoint(
-        "/gpfs/home2/sdejong/pmchhg/dpat/lightning_logs/version_2460498/checkpoints/last.ckpt"  # noqa: E501
-    )
+    # model = SimCLR.load_from_checkpoint(
+    #     "/gpfs/home2/sdejong/pmchhg/dpat/lightning_logs/version_2460498/checkpoints/last.ckpt"  # noqa: E501
+    # )
+    model = SimCLR(pretrained=True)
+    model.to("cuda" if torch.cuda.is_available() else "cpu")
 
     # Make a temporary file make the dataset read from.
     concatenated_file = tempfile.TemporaryFile()
@@ -50,7 +53,15 @@ def main():
         mask_factory="load_from_disk",
         mask_foreground_threshold=1,
         mask_root_dir="/gpfs/home2/sdejong/pmchhg/masks/",
-        transform=Compose([ToTensor()]),
+        transform=Compose(
+            [
+                ToTensor(),
+                Normalize(
+                    # Change to NORMALIZE if not using masks.
+                    **PMCHHGImageDataset.NORMALIZE_MASKED
+                ),
+            ]
+        ),
     )
 
     _ = PMCHHGH5Dataset.from_pmchhg_data_and_model(