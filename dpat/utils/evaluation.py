cb761f2d610ede18c6afeeb13db1c30309e1f642
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2023-04-25 15:06:26 +0200

    fix: add typehints

--- dpat/utils/evaluation.py
+++ dpat/utils/evaluation.py
@@ -1,14 +1,14 @@
 """Evaluation utilities."""
 
-from typing import Sequence
-
 import numpy as np
 import scipy.stats as st
+from numpy.typing import NDArray
 
 
-def compute_mean_and_confidence_interval(values: Sequence, confidence=0.95):
+def compute_mean_and_confidence_interval(
+    values: NDArray, confidence=0.95
+) -> tuple[float, float]:
     """Compute the mean and confidence interval from a sequence of values."""
-    values = np.array(values)
     n = len(values)
     m, se = np.mean(values), st.sem(values)
     h = se * st.t.ppf((1 + confidence) / 2, n - 1)
